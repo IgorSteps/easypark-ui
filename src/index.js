2d85292df3c4eaab25a4be48ed229ec5cfa518d9
Igor Stepanenko <igorstepanenko696@gmail.com>
2024-05-02 19:12:25 +0100

    capital letter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { makeServer } from '../mirageServer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { createRoot } from 'react-dom/client';
-import App from './app.js';
+import App from './App.js';
 
 if (process.env.USING === 'mirage') {
   makeServer();

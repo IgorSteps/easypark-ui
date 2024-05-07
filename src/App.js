621736700c51435aa0d4e2277f49f44de37c7880
Igor Stepanenko <igorstepanenko696@gmail.com>
2024-05-08 00:57:27 +0100

    Admin login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginForm from './views/components/loginForm.js';
 import RegisterForm from './views/components/registerForm.js';
 import DriverDashboard from './views/driverDashboard.js';
+import AdminLoginForm from './views/components/adminLoginForm.js';
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
         <Routes>
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm />} />
-            <Route path="/driver-dashboard" element={<DriverDashboard />} />        
+            <Route path="/driver-dashboard" element={<DriverDashboard />} />     
+
+            <Route path="/admin-login" element={<AdminLoginForm />} />   
         </Routes>
       </BrowserRouter>
   );
